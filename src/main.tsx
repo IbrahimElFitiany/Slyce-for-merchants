6a2d8c6b021a0e1a10be21857d5c601a4421f0c8
Ibrahim El-Fitiany <ibrahimelfitiany@gmail.com>
2026-08-02 03:02:22 +0300

    feat: add TanStack Query provider to app root

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,12 @@ import { createRoot } from 'react-dom/client';
 import { RouterProvider } from "react-router-dom";
 import { router } from "@/Routes/Routes";
 import './index.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+const client = new QueryClient()
 
-createRoot(document.getElementById('root')!).render(
-  <RouterProvider router={router} />
-)
+createRoot(document.getElementById("root")!).render(
+  <QueryClientProvider client={client}>
+    <RouterProvider router={router} />
+  </QueryClientProvider>
+);
